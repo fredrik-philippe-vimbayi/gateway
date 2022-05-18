9fbbfd0a36cb5be1b23264eb2128969f2be2ed41
Vimbayinashe <vimbayinashemandaza@gmail.com>
2022-05-18 16:01:50 +0200

    Add SecurityConstants

---
+++ src/main/java/com/example/gatewayservice/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.gatewayservice.security;
+
+public class SecurityConstants {
+
+    public static final String HEADER_NAME = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String AUTHORITIES_KEY = "roles";
+
+}
